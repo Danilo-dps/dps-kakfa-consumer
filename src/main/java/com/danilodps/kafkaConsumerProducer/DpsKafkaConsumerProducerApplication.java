e888435a9579be8ea23219b157a617626ddb7d25
Danilo-dps <danilodpsnilo@outlook.com>
2026-01-14 00:40:46 -0300

    feat: projeto inicializado

---
+++ src/main/java/com/danilodps/kafkaConsumerProducer/DpsKafkaConsumerProducerApplication.java
@@ -0,0 +1,13 @@
+package com.danilodps.kafkaConsumerProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpsKafkaConsumerProducerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DpsKafkaConsumerProducerApplication.class, args);
+	}
+
+}
